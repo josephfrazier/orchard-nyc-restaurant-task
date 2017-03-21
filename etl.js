990cc37d7180e225871fa3babbe13d675d313b26
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-21 19:04:05 -0400

    Parse CAMIS column into Number

--- etl.js
+++ etl.js
@@ -5,7 +5,11 @@ const etl = require('etl');
 const through2 = require('through2');
 
 fs.createReadStream('/dev/stdin')
-  .pipe(etl.csv())
+  .pipe(etl.csv({
+    transform: {
+      CAMIS: Number,
+    }
+  }))
   .pipe(through2({objectMode: true}, (chunk, enc, callback) => {
     console.log(chunk);
     callback();
