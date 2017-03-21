07490d45cf8dca2cd5969e9a06b140feb593659b
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-21 19:08:44 -0400

    Only emit latest graded record per CAMIS

--- etl.js
+++ etl.js
@@ -4,14 +4,38 @@ const fs = require('fs');
 const etl = require('etl');
 const through2 = require('through2');
 
+let latestGrade;
+
 fs.createReadStream('/dev/stdin')
   .pipe(etl.csv({
     transform: {
       CAMIS: Number,
     }
   }))
+  .pipe(through2({objectMode: true}, filterLatest))
   .pipe(through2({objectMode: true}, (chunk, enc, callback) => {
     console.log(chunk);
     callback();
   }))
   //.pipe(etl.mysql.upsert(pool,'testschema','testtable',{concurrency:4 }))
+
+// keep track of the record with the most recent 'GRADE DATE'
+// when the record's 'CAMIS' changes, push the tracked record
+function filterLatest (record, enc, callback) {
+  if (!latestGrade) {
+    latestGrade = record;
+    return callback();
+  }
+
+  if (record['CAMIS'] != latestGrade['CAMIS']) {
+    this.push(latestGrade);
+    latestGrade = record;
+    return callback();
+  }
+
+  if (Date.parse(record['GRADE DATE']) > Date.parse(latestGrade['GRADE DATE'])) {
+    latestGrade = record;
+  }
+
+  callback();
+}
