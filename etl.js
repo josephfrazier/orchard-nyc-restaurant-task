778612ac73ff3eed5d73b4f94b5877cd39294263
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-21 22:02:21 -0400

    wip

--- etl.js
+++ etl.js
@@ -3,24 +3,72 @@
 const fs = require('fs');
 const etl = require('etl');
 const through2 = require('through2');
+const pg = require('pg');
+const jsonSql = require('json-sql')({
+  dialect: 'postgresql',
+  namedValues: false,
+});
 
 let latestGrade;
 
 main();
 
-function main () {
+async function main () {
+  const pool = new pg.Pool(process.env.DATABASE_URL);
+
+  try {
+    await pool.query('DROP SCHEMA IF EXISTS testschema');
+    await pool.query('CREATE SCHEMA testschema');
+    await pool.query('DROP TABLE IF EXISTS testtable');
+    await pool.query(`CREATE TABLE testtable (
+      "CAMIS" integer,
+      "DBA" text,
+      "BORO" text,
+      "BUILDING" text,
+      "STREET" text,
+      "ZIPCODE" text,
+      "PHONE" text,
+      "CUISINE DESCRIPTION" text,
+      "INSPECTION DATE" date,
+      "ACTION" text,
+      "VIOLATION CODE" text,
+      "VIOLATION DESCRIPTION" text,
+      "CRITICAL FLAG" text,
+      "SCORE" integer,
+      "GRADE" text,
+      "GRADE DATE" date,
+      "RECORD DATE" date,
+      "INSPECTION TYPE" text,
+      "__line" integer
+    )`)
+  } catch (err) {
+    console.trace(err);
+  }
+
   fs.createReadStream('/dev/stdin')
     .pipe(etl.csv({
       transform: {
         CAMIS: Number,
+        'GRADE DATE': date => date.length ? date : null,
+        'RECORD DATE': date => date.length ? date : null,
       }
     }))
     .pipe(through2({objectMode: true}, filterLatest))
-    .pipe(through2({objectMode: true}, (chunk, enc, callback) => {
-      console.log(chunk);
-      callback();
+    .pipe(through2({objectMode: true}, async (chunk, enc, callback) => {
+      // console.error(chunk);
+
+      const sql = jsonSql.build({
+        type: 'insert',
+        table: 'testtable',
+        values: chunk
+      });
+
+      console.error(sql.query, JSON.stringify(sql.values));
+      await pool.query(sql.query, sql.values);
+
+      callback(null, chunk); // TODO remove this pipe entirely
     }))
-    //.pipe(etl.mysql.upsert(pool,'testschema','testtable',{concurrency:4 }))
+    //.pipe(etl.postgres.upsert(pool,'testschema','testtable',{concurrency:4}))
 }
 
 // keep track of the record with the most recent 'GRADE DATE'
