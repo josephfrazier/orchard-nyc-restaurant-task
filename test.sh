9769856932fb296f267299cc584f2beea01afa86
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-22 16:36:02 -0400

    Add `etl` script to package.json that sorts CSV and feeds to etl.js
    Also, don't check for a presorted CSV on disk.

--- test.sh
+++ test.sh
@@ -2,4 +2,3 @@
 set -euo pipefail
 
 sha1sum DOHMH_New_York_City_Restaurant_Inspection_Results.csv | grep 35354fe1fb1adc75b9b484d778c6b4d39c06b1d5
-sha1sum sort_n.csv | grep 1e74e7d78cc112cd9540bd98a9a190993e969760
