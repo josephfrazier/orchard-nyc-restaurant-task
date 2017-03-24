ec8917be13da6cb2ca285a650d517dfe5f646db1
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-24 13:34:36 -0400

    Escape DB values for HTML output

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const pg = require('pg');
 const pgConnectionString = require('pg-connection-string');
 const jsonToTable = require('json-to-table');
 const Case = require('case');
+const escape = require('escape-html');
 
 const pool = new pg.Pool(pgConnectionString.parse(process.env.DATABASE_URL));
 
@@ -20,13 +21,13 @@ router.get('/', async function(req, res, next) {
     <table>
       <thead>
         <tr>
-          ${headers.map(h => `<th>${Case.capital(h)}</th>`).join('')}
+          ${headers.map(h => `<th>${escape(Case.capital(h))}</th>`).join('')}
         </tr>
       </thead>
       <tbody>
         ${rows.map(values => `
           <tr>
-            ${values.map(v => `<td>${v}</td>`).join('')}
+            ${values.map(v => `<td>${escape(v)}</td>`).join('')}
           </tr>
         `).join('')}
       </tbody>
