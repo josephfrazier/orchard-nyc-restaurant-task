fe26909aab7a77e59bc2330dc938da717c65cb82
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-24 13:28:52 -0400

    Render table of top 10 Thai A/B restaurants

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+const pg = require('pg');
+const pgConnectionString = require('pg-connection-string');
+const jsonToTable = require('json-to-table');
+
+const pool = new pg.Pool(pgConnectionString.parse(process.env.DATABASE_URL));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const { rows: restaurants } = await pool.query(`
+    select * from testtable where cuisine_description = 'Thai' and grade in ('A', 'B') order by grade_date desc limit 10;
+  `);
+  const [ headers, ...rows ] = jsonToTable(restaurants);
+
+  // TODO this templating should be in views/index.jade,
+  // but I haven't figured it out quite yet
+  let restaurantsHtml = `
+    <table>
+      <thead>
+        <tr>
+          ${headers.map(h => `<th>${h}</th>`).join('')}
+        </tr>
+      </thead>
+      <tbody>
+        ${rows.map(values => `
+          <tr>
+            ${values.map(v => `<td>${v}</td>`).join('')}
+          </tr>
+        `).join('')}
+      </tbody>
+    </table>
+  `;
+
+  res.render('index', {
+    title: 'Restaurants',
+    restaurantsHtml,
+  });
 });
 
 module.exports = router;
