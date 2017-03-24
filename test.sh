1dcb2bbcb8bbfb59c87ed2e3cf9044890bf84e15
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-24 12:44:10 -0400

    fix test script

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sha1sum DOHMH_New_York_City_Restaurant_Inspection_Results.csv | grep 35354fe1fb1adc75b9b484d778c6b4d39c06b1d5
+sha1sum data/sorted.csv | grep 1e74e7d78cc112cd9540bd98a9a190993e969760
